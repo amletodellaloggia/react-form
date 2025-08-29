06974a0c3ad210b0d94b036c02c2f82ec0fe4c13
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-29 13:42:43 +0200

    [upd] added mb-3 to ul, added form field

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,22 @@ const App = () => {
   return (
     <div className="container mt-4">
       <h2 className="mb-3">Lista Articoli Blog</h2>
-      <ul className="list-group">
+      <ul className="list-group mb-3">
         {BlogArticles.map((article) => (
           <li key={article.id} className="list-group-item">
             {article.title}
           </li>
         ))}
       </ul>
+      <form className="d-flex gap-2">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Nuovo articolo..."
+        />
+      </form>
     </div>
   );
 };
 
-export default App
+export default App;
