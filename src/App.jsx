f95987f842a15b9095c33ea77269e9de50a6947a
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-29 13:37:56 +0200

    [upd] added articles list, added function to show article list objects, added bootstrap classes to render UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-function App({BlogArticles}) {
+// Creo lista articoli
+const BlogArticles = [
+  { id: 1, title: "Coding Issues: How to Understand" },
+  { id: 2, title: "Coding Issues: How to Learn" },
+  { id: 3, title: "Coding Issues: How to Improve" },
+];
 
-
+// Definisco funzione per visualizzare gli articoli staticamente
+const App = () => {
   return (
-    <>
-    <h2>
-      Hello Class#147!
-    </h2>
-    </>
-  )
-}
+    <div className="container mt-4">
+      <h2 className="mb-3">Lista Articoli Blog</h2>
+      <ul className="list-group">
+        {BlogArticles.map((article) => (
+          <li key={article.id} className="list-group-item">
+            {article.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App
